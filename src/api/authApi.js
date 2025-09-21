15bb0bce1a637f9fdfcfa09aab1b1e1ce976d09a
Chittaranjan Tanty <erchitta123@gmail.com>
2025-09-21 23:35:13 +0530

    some bug fixed

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,12 +1,17 @@
 const baseURL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
 
 export async function loginUser(formData) {
+  const urlEncodedData = new URLSearchParams(formData);
+
   const res = await fetch(`${baseURL}/login/login`, {
     method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(formData),
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: urlEncodedData,
     credentials: "include", // important for cookie-based JWT
   });
+
   const data = await res.json();
   if (!res.ok) throw new Error(data.message || "Login failed");
   return data;
